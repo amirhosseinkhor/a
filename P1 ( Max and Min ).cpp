b450becc329055ee05c87192a79f7b33f8de82d8
amirhosseinkhor <ho3ein.13.amir@gmail.com>
2025-03-05 20:40:59 +0330

    Add files via upload

---
+++ P1 ( Max and Min ).cpp
@@ -0,0 +1,42 @@
+/* This is a C++ program which receive some numbers fromyou and as 
+a result it return the max and the min of the recieved numbers */
+
+
+#include <iostream>
+using namespace std;
+
+struct MinMax {
+    int min;
+    int max;
+};
+
+MinMax maxANDmin(int n) {
+    int Number;     // The recieved number
+    cout << "Please enter a number: ";
+    cin >> Number;
+    int max = Number;
+    int min = Number;
+
+    for (int i = 2; i <= n; i++) {
+        cout << "Please enter a number: ";
+        cin >> Number;
+        if (Number > max) {         //here we  reorganize max and min
+            max = Number;
+        } 
+        else if (Number < min) {    //here we  reorganize max and min
+            min = Number;
+        }
+    }
+
+    return {min, max};
+}
+
+int main() {
+    int n;     // The mount of numbers 
+    cout << "Please enter the amount of numbers: ";
+    cin >> n;
+
+    MinMax result = maxANDmin(n);
+    cout << "The min and the max are: " << result.min << " and " << result.max << endl;
+    return 0;
+}
